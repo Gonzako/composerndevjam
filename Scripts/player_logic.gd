ef24c309723050c89ee375996f51725bfc8192ca
sachadow <sachadow.team@gmail.com>
2026-01-14 17:11:37 +0100

    Added simple character animations and added basic AnimationPlayer functionality to go with it

--- Scripts/player_logic.gd
+++ Scripts/player_logic.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @export var move_axis = ["ui_left","ui_right","ui_up","ui_down"]
+@export var animation_player: AnimationPlayer
+var move_animations = ["left", "right", "up", "down"]
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
@@ -15,7 +17,14 @@ func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector(move_axis[0],move_axis[1],move_axis[2],move_axis[3]).normalized()
 	if direction:
 		velocity = direction * SPEED
+		
+		# Simple animation player
+		for index in move_axis.size():
+			if Input.is_action_pressed(move_axis[index]):
+				animation_player.play(move_animations[index])
+		
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO,SPEED*0.2)
+		animation_player.play("idle")
 
 	move_and_slide()
