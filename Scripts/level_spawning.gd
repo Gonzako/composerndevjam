29249cc909953b56801752f74aab6bf2276cd098
GiorgioPortalVBR <gonzako123@gmail.com>
2026-01-15 10:27:34 +0100

    Started testing into basic animator based spawning with music

---
+++ Scripts/level_spawning.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var obstacle : Array[PackedScene]
+@export var bottomNode : Node2D
+@export var topNode : Node2D
+@export var spawnParent : Node2D
+
+func SpawnObstacle(pos_offset: float, obs_idx: int, from_bottom = true) -> void:
+	print("Hello world")
+	var target_obstacle = obstacle[obs_idx].instantiate()
+	target_obstacle.position = bottomNode.position
+	target_obstacle.position.x += pos_offset
+	spawnParent.add_child(target_obstacle)
